c1f25d75fa34b3e7bc53f1a7120cde1deba0f242
ytakasugi <sh7.tibi0129@gmail.com>
2021-05-30 23:12:07 +0900

    change file

--- relly/src/buffer.rs
+++ relly/src/buffer.rs
@@ -21,6 +21,7 @@ pub struct Buffer {
 
 #[derive(Debug, Default)]
 pub struct Frame {
+    // Bufferの利用回数
     usage_count: u64,
     buffer: Rc<Buffer>,
 }
@@ -41,24 +42,34 @@ impl BufferPool {
         }
     }
 
+    // BufferPoolのサイズを取得するメソッド
     fn size(&self) -> usize {
         self.buffers.len()
     }
 
     fn evict(&mut self) -> Option<BufferId> {
+        // BufferPoolサイズの取得
         let pool_size = self.size();
-        let mut consecutive = 0;
+        // `consecutive_pinned`カウンタを初期化
+        let mut consecutive_pinned = 0;
+        // BufferPool内をループし、破棄するBufferを決定する
         let victim_id = loop {
+            // このloop内での`next_victim_id`は、`BufferPool::next_victim_id`に束縛する
             let next_victim_id = self.next_victim_id;
             let frame = &mut self[next_victim_id];
+            // もし、Bufferの利用回数が0だった場合・・・
             if frame.usage_count == 0 {
+                // `next_victim_id`を返す
                 break self.next_victim_id;
             }
+            // もし、Bufferを貸出中でない場合は、Bufferの`usage_count`をデクリメントする
             if Rc::get_mut(&mut frame.buffer).is_some() {
                 frame.usage_count -= 1;
                 consecutive_pinned = 0;
+            // 貸出中の場合、`consecutive_pinned`カウンタをインクリメントする
             } else {
                 consecutive_pinned += 1;
+                // もし、`consecutive_pinned`カウンタがBufferPool以上であった場合は、`None`を返す
                 if consecutive_pinned >= pool_size {
                     return None;
                 }
@@ -69,6 +80,7 @@ impl BufferPool {
     }
 
     fn increment_id(&self, buffer_id: BufferId) -> BufferId {
+        // `BufferId`に1を足して、要素数で割ったあまりを返す
         BufferId((buffer_id.0 + 1) % self.size())
     }
 }
