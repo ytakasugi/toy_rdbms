6bc1b05724a9c687361498117d9b8bbc19c19f36
ytakasugi <sh7.tibi0129@gmail.com>
2021-05-30 21:09:28 +0900

    add buffer.rs

--- relly/src/buffer.rs
+++ relly/src/buffer.rs
@@ -6,8 +6,11 @@ use std::rc::Rc;
 
 use crate::disk::{DiskManager, PageId, PAGE_SIZE};
 
+#[derive(Debug, Default, Clone, Copy, Eq, PartialEq, Hash)]
+pub struct BufferId(usize);
+
 // `u8`型の配列
-pub type PAGE = [u8; PAGE_SIZE];
+pub type Page = [u8; PAGE_SIZE];
 
 #[derive(Debug)]
 pub struct Buffer {
@@ -25,4 +28,53 @@ pub struct Frame {
 pub struct BufferPool {
     buffers: Vec<Frame>,
     next_victim_id: BufferId,
+}
+
+impl BufferPool {
+    pub fn new(pool_size: usize) -> Self {
+        let mut buffers = vec![];
+        buffers.resize_with(pool_size, Default::default);
+        let next_victim_id = BufferId::default();
+        Self {
+            buffers,
+            next_victim_id,
+        }
+    }
+
+    fn size(&self) -> usize {
+        self.buffers.len()
+    }
+
+    fn evict(&mut self) -> Option<BufferId> {
+        let pool_size = self.size();
+        let mut consecutive = 0;
+        let victim_id = loop {
+            let next_victim_id = self.next_victim_id;
+            let frame = &mut self[next_victim_id];
+            if frame.usage_count == 0 {
+                break self.next_victim_id;
+            }
+            if Rc::get_mut(&mut frame.buffer).is_some() {
+                frame.usage_count -= 1;
+                consecutive_pinned = 0;
+            } else {
+                consecutive_pinned += 1;
+                if consecutive_pinned >= pool_size {
+                    return None;
+                }
+            }
+            self.next_victim_id = self.increment_id(self.next_victim_id);
+        };
+        Some(victim_id)
+    }
+
+    fn increment_id(&self, buffer_id: BufferId) -> BufferId {
+        BufferId((buffer_id.0 + 1) % self.size())
+    }
+}
+
+pub struct BufferPoolManager {
+    disk: DiskManager,
+    pool: BufferPool,
+    page_table: HashMap<PageId, BufferId>,
 }
